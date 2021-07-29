f2136298f13e994f3d8bc1ab78c8a492549187e1
luomiao <1084459061@qq.com>
2021-07-30 02:01:00 +0800

    vue-router二次封装(10)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="app">
-	<el-row :gutter="20">
-		<el-col :span="6">1</el-col>
-		<el-col :span="6">2</el-col>
-		<el-col :span="6">3</el-col>
-		<el-col :span="6">4</el-col>
-	</el-row>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
+<script>
+	export default {
+		
+	}
+</script>
+
 
 <style lang="scss">
 
---
+++ src/views/shop/goods/list.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>商品列表页</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>

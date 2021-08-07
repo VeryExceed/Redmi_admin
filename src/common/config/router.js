7ad8172cf3579734f9d53d5d9e7f38274bf7f86e
luomiao <1084459061@qq.com>
2021-08-07 10:34:38 +0800

    表单构建-商品规格（61）

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -25,8 +25,16 @@ let routes = [{
 				component: 'shop/goods/list'
 			},
 			{
-				meta:{title:'相册管理'},
-				component:'image/index'
+				meta: {
+					title: '创建商品'
+				},
+				component: 'shop/goods/create'
+			},
+			{
+				meta: {
+					title: '相册管理'
+				},
+				component: 'image/index'
 			}
 		]
 	},

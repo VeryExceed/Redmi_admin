b946ca2de75d3cce051a544507451bd9d4955399
luomiao <1084459061@qq.com>
2021-09-07 08:23:43 +0800

    增加顶级分类功能（172）

--- src/views/shop/category/list.vue
+++ src/views/shop/category/list.vue
@@ -9,7 +9,7 @@
 			:expand-on-click-node="false" draggable @node-drop="nodeDrop">
 			<span class="custom-tree-node" slot-scope="{ node, data }">
 				<div>
-					<el-input v-if="data.editStatus" size="mini" v-model="data.label"></el-input>
+					<el-input v-if="data.editStatus" size="mini" v-model="data.name"></el-input>
 					<span v-else>{{ node.label }}</span>
 				</div>
 				<span>
@@ -29,47 +29,30 @@
 
 <script>
 	export default {
-		inject:['layout'],
+		inject: ['layout'],
 		data() {
 			return {
-				data: [{
-					id: 1,
-					label: '一级 1',
-					status: 1,
-					editStatus: false,
-					children: [{
-						id: 2,
-						label: '二级 1-1',
-						status: 1,
-						editStatus: false,
-						children: [{
-							id: 3,
-							label: '三级 1-1-1',
-							status: 1,
-							editStatus: false,
-						}]
-					}]
-				}],
+				data: [],
 				defaultProps: {
-					children: 'children',
-					label: 'label'
+					children: 'child',
+					label: 'name'
 				}
 			}
 		},
-		created(){
+		created() {
 			this.__init()
 		},
 		methods: {
 			// 初始化
-			__init(){
+			__init() {
 				this.layout.showLoading()
-				this.axios.get('/admin/category',{
-					token:true
-				}).then(res=>{
+				this.axios.get('/admin/category', {
+					token: true
+				}).then(res => {
 					console.log(res)
 					let data = res.data.data
-					let addEditStatus = function(arr){
-						arr.forEach(item=>{
+					let addEditStatus = function(arr) {
+						arr.forEach(item => {
 							item.editStatus = false
 							if (item.child.length) {
 								addEditStatus(item.child)
@@ -79,7 +62,7 @@
 					addEditStatus(data)
 					this.data = data
 					this.layout.hideLoading()
-				}).catch(err=>{
+				}).catch(err => {
 					this.layout.hideLoading()
 				})
 			},
@@ -88,11 +71,52 @@
 			},
 			// 显示/隐藏
 			showOrHide(data) {
-				data.status = data.status ? 0 : 1
+				this.layout.showLoading()
+				let status = data.status ? 0 : 1
+				let msg = status ? '显示' : '隐藏'
+				this.axios.post('/admin/category/' + data.id + '/update_status', {
+					status
+				}, {
+					token: true
+				}).then(res => {
+					data.status = status
+					this.layout.hideLoading()
+					this.$message({
+						message: msg + '成功',
+						type: 'success'
+					})
+				}).catch(err => {
+					this.layout.hideLoading()
+				})
 			},
 			// 编辑|提交
 			edit(data) {
-				data.editStatus = !data.editStatus
+				if (!data.editStatus) {
+					return data.editStatus = true
+				}
+				if (data.name == '') {
+					return this.$message({
+						message: '分类名称不能为空',
+						type: 'error'
+					});
+				}
+				this.layout.showLoading()
+				this.axios.post('/admin/category/' + data.id, {
+					status: data.status,
+					name: data.name,
+					category_id: data.category_id
+				}, {
+					token: true
+				}).then(res => {
+					this.$message({
+						message: '修改成功',
+						type: 'success'
+					});
+					data.editStatus = false
+					this.layout.hideLoading()
+				}).catch(err => {
+					this.layout.hideLoading()
+				})
 			},
 			// 删除节点分类
 			remove(node, data) {
@@ -101,25 +125,40 @@
 					cancelButtonText: '取消',
 					type: 'warning'
 				}).then(() => {
-					let parent = node.parent
-					let children = parent.data.children || parent.data
-					let index = children.findIndex(v => {
-						return v.id === data.id
+					this.layout.showLoading()
+					this.axios.delete('/admin/category/'+data.id,{
+						token:true
+					}).then(res=>{
+						this.__init()
+						this.$message({
+							message:'删除成功',
+							type:'success'
+						})
+						this.layout.hideLoading()
+					}).catch(err=>{
+						this.layout.hideLoading()
 					})
-					children.splice(index, 1)
 				})
 
 			},
 			// 增加
 			append(data) {
-				let newObj = {
-					id: 2,
-					label: '二级 1-1',
-					status: 1,
-					editStatus: true,
-					children: []
-				}
-				data.children.push(newObj)
+				this.layout.showLoading()
+				this.axios.post('/admin/category',{
+					status:0,
+					name:'新分类',
+					category_id:data.id
+				},{
+					token:true
+				}).then(res=>{
+					let obj = res.data.data
+					obj.editStatus = true
+					obj.child = []
+					data.child.push(obj)
+					this.layout.hideLoading()
+				}).catch(err=>{
+					this.layout.hideLoading()
+				})
 			},
 			// 拖拽
 			nodeDrop(...options) { //扩展运算里面的参数
@@ -140,13 +179,24 @@
 				}).then(({
 					value
 				}) => {
-					console.log(value)
-				}).catch(() => {
-					this.$message({
-						type: 'info',
-						message: '取消创建'
-					});
-				});
+					this.layout.showLoading()
+					this.axios.post('/admin/category',{
+						status:0,
+						category_id:0,
+						name:value
+					},{
+						token:true
+					}).then(res=>{
+						this.__init()
+						this.$message({
+							message:'创建成功',
+							type:'success'
+						})
+						this.layout.hideLoading()
+					}).catch(err=>{
+						this.layout.hideLoading()
+					})
+				})
 			}
 		}
 	}
